0e7cded8ed02bb03353837869ffab9f6d1360417
Domestic-Mouse <113106213+Domestic-Mouse@users.noreply.github.com>
2022-10-06 09:22:12 +0100

    Update school project.py

--- school project.py
+++ school project.py
@@ -14,16 +14,13 @@ pygame.init
 def core(): 
   #Creates key variables
   window = pygame.display.set_mode([1250,750])
-  totalFX = 0
-  totalFY = 0
-  AU = 384400000 # distance from moon to Earth in M
-  scale = (300/AU)
   running = True
   Orbit_moon = ((3600*24)/14)
   clock = pygame.time.Clock()
   Pause = False
   # initalises the class for the bodies
   class body(): 
+    #class Earth(body):
     def __init__(self,mass,radius,xposition,yposition,colour,xvelocity,yvelocity):
       self.mass = mass
       self.rad = radius
@@ -33,10 +30,12 @@ def core():
       self.xvel = xvelocity # determines the x and y velocity of the planet
       self.yvel = yvelocity
       self.Gravconst = 6.68*10**-11
+      self.AU = 384400000 
+      self.scale = 300/self.AU
 
     def positionUpdate(self,window):
-      y = self.ypos * scale + 375
-      x = self.xpos * scale + 625
+      y = self.ypos * self.scale + 375
+      x = self.xpos * self.scale + 625
       print(x,y)
       pygame.draw.circle(window,self.colour,(x,y),self.rad)
       pygame.draw.line(window,((255,255,255)),(planet1.xpos,planet1.ypos),(x,y))
@@ -44,19 +43,26 @@ def core():
     def calculation(self,other):
       xdistance =  (self.xpos - other.xpos)
       ydistance =  (self.ypos - other.ypos)
+      print(xdistance,ydistance)
       distance = math.sqrt(xdistance**2 + ydistance**2)
       force = ((self.Gravconst*self.mass*other.mass)/distance**2)
       angle = math.atan2(xdistance,ydistance)
       forceX = math.cos(angle) * force
       forceY = math.sin(angle) * force
-      return forceX, forceY
+      planet2.xvel += forceX/planet2.mass*Orbit_moon 
+      planet2.yvel += forceY/planet2.mass*Orbit_moon 
+      angVel = (planet2.xvel**2+planet2.yvel**2)/self.AU
+      planet2.yvel += angVel*math.cos(angle+pi/2)
+      planet2.xvel += angVel*math.sin(angle+pi/2)
+      x = planet2.xvel
+      y = planet2.yvel
+      return x, y
     
-    #page 66 kerboodle for keplar
 
 
   #defines 2 bodies
   planet1 = body(5.9*10**24,6,625,375,((0,0,255)),0,0)
-  planet2 = body(7.3*10**22,4,-1*AU,0,((255,255,255)),0,0)
+  planet2 = body(7.3*10**22,4,-1*384400000,0,((255,255,255)),0,0)
   pygame.display.set_caption("Orbit Simulation")
   
   while running:
@@ -93,14 +99,10 @@ def core():
 
     
     #bulk of calculations
-    forceX, forceY = planet2.calculation(planet1)
-    totalFX += forceX
-    totalFY += forceY
-    planet2.xvel += totalFX/planet2.mass*Orbit_moon
-    planet2.yvel += totalFY/planet2.mass*Orbit_moon
-    print(math.sqrt(planet2.xvel**2+planet2.yvel**2))
-    planet2.xpos += (planet2.xvel * Orbit_moon)
-    planet2.ypos += (planet2.yvel * Orbit_moon)
+    planet2.xpos, planet2.ypos = planet2.calculation(planet1)
+    
+    planet2.xpos *= Orbit_moon
+    planet2.ypos *= Orbit_moon
     planet2.positionUpdate(window)
     
     
